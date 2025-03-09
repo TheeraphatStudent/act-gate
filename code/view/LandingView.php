884805d5388155f97e9c75e15e77af8111567ed1
TheeraphatCH <theeraphatchueanokkhum@gmail.com>
2025-03-09 17:59:28 +0700

    feat: update event detail and style

--- code/view/LandingView.php
+++ code/view/LandingView.php
@@ -25,126 +25,6 @@ $calendar = new SchedulerCalendar();
 
     <link rel="shortcut icon" type="image/x-icon" href="public/images/logo.png">
     <link rel="stylesheet" href="public/style/main.css">
-
-    <style>
-        .text-container {
-            position: relative;
-        }
-
-        .animated-text {
-            font-size: 40px;
-            font-weight: bold;
-            display: inline-block;
-            text-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
-            animation: float 3s ease-in-out infinite;
-        }
-
-        .animated-text span {
-            display: inline-block;
-            animation: colorChange 3s infinite;
-        }
-
-        .animated-text span:nth-child(1) {
-            animation-delay: 0.1s;
-        }
-
-        .animated-text span:nth-child(2) {
-            animation-delay: 0.2s;
-        }
-
-        .animated-text span:nth-child(3) {
-            animation-delay: 0.3s;
-        }
-
-        .animated-text span:nth-child(4) {
-            animation-delay: 0.4s;
-        }
-
-        .animated-text span:nth-child(5) {
-            animation-delay: 0.5s;
-        }
-
-        .animated-text span:nth-child(6) {
-            animation-delay: 0.6s;
-        }
-
-        .animated-text span:nth-child(7) {
-            animation-delay: 0.7s;
-        }
-
-        .animated-text span:nth-child(8) {
-            animation-delay: 0.8s;
-        }
-
-        .animated-text span:nth-child(9) {
-            animation-delay: 0.9s;
-        }
-
-        .animated-text span:nth-child(10) {
-            animation-delay: 1.0s;
-        }
-
-        .animated-text span:nth-child(11) {
-            animation-delay: 1.1s;
-        }
-
-        .animated-text span:nth-child(12) {
-            animation-delay: 1.2s;
-        }
-
-        @keyframes colorChange {
-            0% {
-                color: #BCFFAB;
-                transform: scale(1) rotate(0deg);
-            }
-
-            25% {
-                color: #ABCDFF;
-                transform: scale(1.1) rotate(5deg);
-            }
-
-            50% {
-                color: #FBF8EE;
-                transform: scale(1) rotate(0deg);
-            }
-
-            75% {
-                color: #ABCDFF;
-                transform: scale(1.1) rotate(-5deg);
-            }
-
-            100% {
-                color: #BCFFAB;
-                transform: scale(1) rotate(0deg);
-            }
-        }
-
-        @keyframes float {
-            0% {
-                transform: translateY(0px);
-            }
-
-            50% {
-                transform: translateY(-15px);
-            }
-
-            100% {
-                transform: translateY(0px);
-            }
-        }
-
-        .glow {
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            top: 0;
-            left: 0;
-            background: radial-gradient(circle, rgba(188, 255, 171, 0.3) 0%, rgba(0, 83, 186, 0.1) 50%, rgba(26, 26, 46, 0) 70%);
-            filter: blur(20px);
-            z-index: -1;
-            animation: pulse 4s infinite;
-        }
-    </style>
 </head>
 
 <body class="flex flex-col justify-center items-center bg-primary relative gap-12">
@@ -164,7 +44,9 @@ $calendar = new SchedulerCalendar();
                 class="flex flex-row justify-center items-center gap-2.5 pt-2.5 pr-2.5 pb-2.5 pl-2.5 rounded-3xl h-60 bg-gradient-to-b from-[rgba(251,248,238,0)] from-0% to-[rgba(34,110,106,1)] to-100% min-w-[1650px]">
                 <div
                     class="font-kanit lg:text-4xl text-2xl w-fit whitespace-nowrap text-white text-opacity-100 leading-none text-dark/primary">
-                    Create events, invite, and connect easily!
+                    <span class="typing-animation max-w-fit">
+                        Create events, invite, and connect easily!
+                    </span>
                 </div>
             </div>
 
@@ -185,11 +67,11 @@ $calendar = new SchedulerCalendar();
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-content h-full min-h-fit mx-10 px-5 lg:px-16">
                 <?php foreach ($allEvents as $item) :
                     // $dataUrl = "data:image/png;base64," . base64_encode(file_get_contents($item['cover']));
-                    // print_r($dataUrl);
+                    // print_r($item);
                 ?>
-                    <div class="flex flex-col justify-between items-center p-4 gap-4 rounded-lg w-full h-[420px] shadow-md bg-white hover:scale-105 hover:cursor-pointer">
+                    <div class="flex flex-col justify-between items-center p-4 gap-4 rounded-lg w-full h-[420px] shadow-md bg-white hover:scale-[101%] hover:cursor-pointer">
                         <div
-                            class="flex flex-col justify-between items-center bg-center bg-cover gap-[150px] rounded w-full h-60 overflow-hidden bg-dark-primary/50 border-dashed border-gray-400 border-2"
+                            class="flex flex-col justify-between items-stretch bg-center bg-cover rounded w-full h-60 overflow-hidden bg-dark-primary/50 border-dashed border-gray-400 border-2"
                             style="background-image: url(public/images/uploads/<?= $item['cover'] ?>);">
                             <!-- Tag -->
                             <div class="flex flex-row justify-start items-start gap-2.5 p-2.5 pb-3.5 w-full h-fit bg-gradient-to-b from-dark-primary/50 via-dark-primary/25 to-transparent">
@@ -227,12 +109,15 @@ $calendar = new SchedulerCalendar();
                                 <?php endforeach ?>
                             </div>
 
-                            <!-- Star -->
-                            <!-- <div class="flex flex-row justify-end items-center gap-2.5 pt-2.5 pr-2.5 pb-2.5 pl-2.5 w-full h-11">
-                                    <div class="flex flex-row justify-center items-center gap-2.5 rounded-[50px] h-7 border-white border-t border-b border-l border-r border-dashed border overflow-hidden">
-                                        <img src="/assets/ImageAsset10.png" alt="Image Asset 10" width="20px" height="20px" />
-                                    </div>
-                                </div> -->
+                            <!-- Organize Detail -->
+                            <div class="flex flex-row justify-start items-center gap-2.5 px-2.5 py-2 w-full h-fit">
+                                <div class="w-[32px] h-[32px] flex items-center justify-center rounded-full <?= (isset($_SESSION['user']['userId']) && $item['organizeId'] === $_SESSION['user']['userId']) ? 'bg-dark-secondary' : 'bg-primary' ?> text-white text-sm font-bold">
+                                    <?= htmlspecialchars(strtoupper(substr($item['organizeName'], 0, 1))) ?>
+                                </div>
+                                <span class="text-white text-sm">
+                                    <?= htmlspecialchars($item['organizeName']) ?>
+                                </span>
+                            </div>
                         </div>
 
                         <div class="flex flex-col justify-start items-start gap-3 h-24 w-full">
@@ -242,7 +127,7 @@ $calendar = new SchedulerCalendar();
                                         <?= htmlspecialchars_decode($item['title'] ?? "-") ?>
                                     </div>
                                     <div class="font-kanit text-lg text-right whitespace-nowrap text-black leading-none font-normal">
-                                        <?= htmlspecialchars_decode($item['maximum'] ?? "-") ?>
+                                        <?= htmlspecialchars_decode($item['maximum'] === -1 ? "-" : $item['maximum']) ?>/<?= htmlspecialchars_decode($item['joined'] ?? "0") ?>
                                     </div>
                                 </div>
                                 <div class="flex flex-col gap-1 font-kanit text-sm w-full whitespace-nowrap text-primary leading-none font-normal">
@@ -251,7 +136,7 @@ $calendar = new SchedulerCalendar();
                                 </div>
                             </div>
                         </div>
-                        <div class="flex flex-row justify-center items-center gap-2.5 w-full h-9">
+                        <div class="flex flex-row justify-center items-center gap-2.5 w-full h-10">
                             <a href="../?action=event.attendee&id=<?= $item['eventId'] ?>" class="btn-primary max-h-10 w-full max-w-[80%]">
                                 <span class="font-kanit text-base text-white">
                                     ดูกิจกรรม
